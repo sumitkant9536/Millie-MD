9e73a39cfe6491c0f0f8bad7b485d59d8ef250f6
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-04 19:53:31 +0530

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -5,8 +5,7 @@ let handler = async (m, { conn, text }) => {
   if (!res.ok) throw await `${res.status} ${res.statusText}`
   let json = await res.json()
   if (!json.url) throw 'Media not found!'
-  if (json.nsfw && !db.data.settings.nsfw) throw 'NSFW mode off'
-
+  
   await conn.sendFile(m.chat, json.url, text, json.title, m, false, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['subreddit <search>']
